863f92462fab7857a461cd361844a95f8a31944c
M-FahadQasim <fahadqasim578@gmail.com>
2026-02-06 10:57:07 +0000

    variables var,const,let

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const accountid = 1234567
+let name1 ="Fahad"
+var email="fahad@gmail.com"
+city="Islamabad"
+let state
+console.log(accountid)
+name1 ="Ali"
+var email="ali@gmail.com"
+city="lahore"
+console.table([accountid,name1,email,city,state])
