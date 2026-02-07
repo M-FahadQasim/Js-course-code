9ccfde3e82b295c0b22470b53cde3fbdd342c502
M-FahadQasim <fahadqasim578@gmail.com>
2026-02-07 08:52:32 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//state that we are using latest js
+//alert(3+3)
+/* couldn't use in node like in brower or in html
+bind file*/
+// Also code readablity and future poof in is piority
+console.log(3
+    +3 //could be written but not a good practice
+);
+console.log("Fahad");
+
+let name = "Fahad"
+let age = 5
+let login = true
+//NUMBER limit in 2^51 or 53
+// BigInt 
+// string =>""
+// boolean => True/False
+// null => stand alone values
+// undefined => no defined
+//symbol => unique
+//object 
+console.log(typeof age); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
